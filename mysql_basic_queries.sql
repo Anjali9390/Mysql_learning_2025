41e2869b78b8fff2a2927988b89b84f5711e9cce
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-09-22 11:20:36 +0530

    Update mysql_basic_queries.sql

--- mysql_basic_queries.sql
+++ mysql_basic_queries.sql
@@ -1,1 +1,9 @@
-SELECT * FROM employee;
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    department VARCHAR(50),
+    salary DECIMAL(10,2),
+    experience VARCHAR(20),
+    age INT
+);
+
